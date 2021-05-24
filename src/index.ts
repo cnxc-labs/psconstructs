e02f6861f2a480b81b0fb96183b4fa8864eeca04
Ken Papagno <kenneth.papagno@concentrix.com>
2021-05-24 09:10:00 -0400

    feat: securebucket

--- src/index.ts
+++ src/index.ts
@@ -29,8 +29,6 @@ export class SecureBucket extends cdk.Construct {
   constructor (scope: cdk.Construct, id:string, attrs: s3.BucketProps) {
     super(scope, id);
 
-    // bump2
-
     attrs = _.defaults({}, attrs, {
       encryption: s3.BucketEncryption.KMS,
       encryptionKey: kms.Alias.fromAliasName(this, 's3AliasKey', 'alias/aws/s3'),
